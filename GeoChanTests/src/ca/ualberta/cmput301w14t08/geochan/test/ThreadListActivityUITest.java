af63e827c35af1c9c867835c53d643c6d5c805be
Artem Herasymchuk <artemh@gmail.com>
2014-02-13 15:33:46 -0700

    Added dummy tests

---
+++ GeoChanTests/src/ca/ualberta/cmput301w14t08/geochan/test/ThreadListActivityUITest.java
@@ -0,0 +1,21 @@
+package ca.ualberta.cmput301w14t08.geochan.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import ca.ualberta.cmput301w14t08.geochan.ThreadListActivity;
+
+public class ThreadListActivityUITest extends
+		ActivityInstrumentationTestCase2<ThreadListActivity> {
+
+	public ThreadListActivityUITest() {
+		super(ThreadListActivity.class);
+	}
+	
+	public void testDummyPass() {
+		//
+	}
+	
+	public void testDummyFail() {
+		fail("Fail");
+	}
+
+}
