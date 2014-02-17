f3656d1ae1f13182ba5891b4fb2c886470336843
Artem Herasymchuk <artemh@gmail.com>
2014-02-16 20:39:53 -0700

    formatting/style

--- GeoChanTests/src/ca/ualberta/cmput301w14t08/geochan/test/ThreadListActivityUITest.java
+++ GeoChanTests/src/ca/ualberta/cmput301w14t08/geochan/test/ThreadListActivityUITest.java
@@ -3,18 +3,17 @@ package ca.ualberta.cmput301w14t08.geochan.test;
 import android.test.ActivityInstrumentationTestCase2;
 import ca.ualberta.cmput301w14t08.geochan.ThreadListActivity;
 
-public class ThreadListActivityUITest extends
-		ActivityInstrumentationTestCase2<ThreadListActivity> {
+public class ThreadListActivityUITest extends ActivityInstrumentationTestCase2<ThreadListActivity> {
 
-	public ThreadListActivityUITest() {
-		super(ThreadListActivity.class);
-	}
+    public ThreadListActivityUITest() {
+        super(ThreadListActivity.class);
+    }
 
-	public void testDummyFail() {
-		fail("LOL");
-	}
-	
-	public void testDummyPass() {
-		assertEquals("1 equals 1", 1, 1);
-	}
+    public void testDummyFail() {
+        fail("LOL");
+    }
+
+    public void testDummyPass() {
+        assertEquals("1 equals 1", 1, 1);
+    }
 }
