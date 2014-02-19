65e756b1a478c2fc27888bbf0705cd841cd16456
Artem Herasymchuk <artemh@gmail.com>
2014-02-19 15:00:22 -0700

    begin work on post comment tests

--- GeoChanTests/src/ca/ualberta/cmput301w14t08/geochan/test/CommentTest.java
+++ GeoChanTests/src/ca/ualberta/cmput301w14t08/geochan/test/CommentTest.java
@@ -7,14 +7,14 @@ import ca.ualberta.cmput301w14t08.geochan.Comment;
 public class CommentTest extends TestCase {
 
     public void testHasImage() {
-        Comment comment = new Comment("test",new Picture(),null);
-        assertTrue("Comment has image",comment.hasImage());
+        Comment comment = new Comment("test", new Picture(), null);
+        assertTrue("Comment has image", comment.hasImage());
     }
 
     @SuppressWarnings("unused")
     public void testAddChild() {
         Comment parent = new Comment("test", null);
-        Comment reply = new Comment("test_reply",null,parent);
+        Comment reply = new Comment("test_reply", null, parent);
         assertNotNull("comment has a reply", parent.getChildren());
     }
 
