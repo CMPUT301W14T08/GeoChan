973061384a6896fb54af6e7d7ea6f434f0aaf76b
artemcm <artem@ualberta.ca>
2014-02-19 20:59:13 -0700

    added a ThreadListActivity test

--- GeoChanTests/src/ca/ualberta/cmput301w14t08/geochan/test/ThreadListActivityUITest.java
+++ GeoChanTests/src/ca/ualberta/cmput301w14t08/geochan/test/ThreadListActivityUITest.java
@@ -1,6 +1,11 @@
 package ca.ualberta.cmput301w14t08.geochan.test;
 
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 import ca.ualberta.cmput301w14t08.geochan.ThreadListActivity;
 
 public class ThreadListActivityUITest extends ActivityInstrumentationTestCase2<ThreadListActivity> {
@@ -15,5 +20,14 @@ public class ThreadListActivityUITest extends ActivityInstrumentationTestCase2<T
 
     public void testDummyPass() {
         assertEquals("1 equals 1", 1, 1);
+    }
+    
+    public void testListViewVisibility() {
+        Intent intent = new Intent();
+        setActivityIntent(intent);
+        ThreadListActivity activity = getActivity();
+        ListView listView = (ListView) activity.findViewById(ca.ualberta.cmput301w14t08.geochan.R.id.thread_list);
+        View rootView = activity.getWindow().getDecorView();
+        ViewAsserts.assertOnScreen(rootView, listView);
     }
 }
