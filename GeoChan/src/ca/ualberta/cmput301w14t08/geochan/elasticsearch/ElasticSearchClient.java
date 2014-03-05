6927531e6048c162446d6b8aa0b1d86ed220536d
Artem Herasymchuk <artemh@gmail.com>
2014-03-05 13:03:17 -0700

    Begin work on elasticsearch client

---
+++ GeoChan/src/ca/ualberta/cmput301w14t08/geochan/elasticsearch/ElasticSearchClient.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 Artem Chikin
+ * Copyright 2014 Artem Herasymchuk
+ * Copyright 2014 Tom Krywitsky
+ * Copyright 2014 Henry Pabst
+ * Copyright 2014 Bradley Simons
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.ualberta.cmput301w14t08.geochan.elasticsearch;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.ClientConfig;
+
+public class ElasticSearchClient {
+    private static ElasticSearchClient instance = null;
+    private static JestClient client;
+    private static final String URL = "http://cmput301.softwareprocess.es:8080/testing";
+    
+    private ElasticSearchClient() {
+        ClientConfig config = new ClientConfig.Builder(URL).multiThreaded(true).build();
+        JestClientFactory factory = new JestClientFactory();
+        factory.setClientConfig(config);
+        client = factory.getObject();
+    }
+    
+    public static ElasticSearchClient getInstance() {
+        if (instance == null) {
+            instance = new ElasticSearchClient();
+        }
+        return instance;
+    }
+}
