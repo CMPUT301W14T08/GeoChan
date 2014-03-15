5f80f15f84db03cc1e512422649bb3126a40532e
Artem Herasymchuk <artemh@gmail.com>
2014-03-15 06:51:31 -0600

    Update ElasticSearchResponse.java

--- GeoChan/src/ca/ualberta/cmput301w14t08/geochan/elasticsearch/ElasticSearchResponse.java
+++ GeoChan/src/ca/ualberta/cmput301w14t08/geochan/elasticsearch/ElasticSearchResponse.java
@@ -17,6 +17,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+  
+ /*
+  * Reused from https://github.com/rayzhangcl/ESDemo/
+  */
 
 package ca.ualberta.cmput301w14t08.geochan.elasticsearch;
 
