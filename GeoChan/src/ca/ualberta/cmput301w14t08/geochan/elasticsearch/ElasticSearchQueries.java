e4fb8b8a7a98375314b20c5470b357f3dab05905
Artem Herasymchuk <artemh@gmail.com>
2014-03-21 14:57:23 -0600

    Search for all threads

--- GeoChan/src/ca/ualberta/cmput301w14t08/geochan/elasticsearch/ElasticSearchQueries.java
+++ GeoChan/src/ca/ualberta/cmput301w14t08/geochan/elasticsearch/ElasticSearchQueries.java
@@ -29,7 +29,7 @@ public class ElasticSearchQueries {
     /**
      * A query string to search for all results in a type.
      */
-    public static final String SEARCH_MATCH_ALL = "{\n" + "   \"query\": {\n"
+    public static final String SEARCH_MATCH_ALL = "{\n" + "   \"size\" : 9999,  \n" + "   \"query\": {\n"
             + "       \"match_all\" : { } \n" + "   }\n" + "}";
 
     private static final String SEARCH_MATCH_PARENT_BEGIN = "{\n" + "   \"query\": {\n"
