8f7a2c3aeb53afcc4af0c3baa12e8818bb55c13f
bradleyjsimons <simons@ualberta.ca>
2014-03-21 16:23:24 -0600

    Minor changes to zoom on map view

--- GeoChan/src/ca/ualberta/cmput301w14t08/geochan/fragments/MapViewFragment.java
+++ GeoChan/src/ca/ualberta/cmput301w14t08/geochan/fragments/MapViewFragment.java
@@ -1,7 +1,6 @@
 package ca.ualberta.cmput301w14t08.geochan.fragments;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.bonuspack.overlays.Marker;
@@ -12,7 +11,6 @@ import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.Overlay;
 
 import android.app.Activity;
 import android.app.Fragment;
@@ -20,6 +18,7 @@ import android.app.FragmentManager;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -44,12 +43,12 @@ public class MapViewFragment extends Fragment {
     private Polyline roadOverlay;
     private Comment topComment;
     
+    private double MAX_LAT;
+    private double MAX_LONG;
+    private double MIN_LAT;
+    private double MIN_LONG;
     
-    private double DELTA_LAT;
-    private double DELTA_LONG;
-    
-    int latSpan;
-    int longSpan;
+    final public static int ZOOM_FACTOR = 900000;
 
     class MapAsyncTask extends AsyncTask<Void,Void,Void> {
 
@@ -109,16 +108,18 @@ public class MapViewFragment extends Fragment {
 
         activity = getActivity();
         
-        DELTA_LAT = 0;
-        DELTA_LONG = 0;
-
+       
         locationListenerService = new LocationListenerService(activity);
         locationListenerService.startListening();
-
         currentLocation = new GeoLocation(locationListenerService);
 
         Bundle args = getArguments();
         topComment = (Comment) args.getParcelable("thread_comment");
+        
+        MAX_LAT = topComment.getLocation().getLatitude();
+        MIN_LAT = topComment.getLocation().getLatitude();
+        MAX_LONG = topComment.getLocation().getLongitude();
+        MIN_LONG = topComment.getLocation().getLongitude();
         
         GeoLocation geoLocation = topComment.getLocation();
         if (geoLocation.getLocation() == null) {
@@ -135,15 +136,10 @@ public class MapViewFragment extends Fragment {
         super.onStop();
         locationListenerService.stopListening();
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
     
     /**
      * This setups up the comment location the map. The map is center at the location
-     * of the comment GeoLocation, and has a bubble on this point/
+     * of the comment GeoLocation, and has a bubble on this point
      * @param geoLocation
      */
     public void setupMap(Comment comment) {
@@ -155,22 +151,36 @@ public class MapViewFragment extends Fragment {
         GeoLocation geoLocation = comment.getLocation();
         startGeoPoint = new GeoPoint(geoLocation.getLocation());
         setGeoPointMarker(startGeoPoint);
-        buildMarkersForChildComments(comment);
+        handleChildComments(comment);
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        int maxLatitude = (int) Math.round(MAX_LAT);
+        int maxLongitude = (int) Math.round(MAX_LONG);
+        int minLatitude = (int) Math.round(MAX_LAT);
+        int minLongitude = (int) Math.round(MAX_LONG);
         
-        latSpan = (int) Math.round(DELTA_LAT);
-        longSpan = (int) Math.round(DELTA_LONG);
+        Log.e("MaxLat", Integer.toString(maxLatitude));
+        Log.e("MInLat", Integer.toString(minLatitude));
+        Log.e("MaxLong", Integer.toString(maxLongitude));
+        Log.e("MinLong", Integer.toString(minLongitude));
+        Log.e("zoomToSpanLong",Integer.toString(Math.round(maxLongitude - minLongitude) * ZOOM_FACTOR));
+        Log.e("zoomToSpanLat",Integer.toString(Math.round(maxLatitude - minLatitude) * ZOOM_FACTOR));
         
         mapController = openMapView.getController();
-        mapController.setZoom(12);
-
+        mapController.setZoom(18);
         mapController.animateTo(startGeoPoint);
+        mapController.zoomToSpan(Math.round(maxLongitude - minLongitude) * ZOOM_FACTOR,
+                Math.round(maxLatitude - minLatitude) * ZOOM_FACTOR);
     }
     
     /**
      * recursively places markers on the map of all child comments of a thread.
      * @param comment
      */
-    private void buildMarkersForChildComments(Comment comment) {
+    private void handleChildComments(Comment comment) {
         ArrayList<Comment> children = comment.getChildren();
         if (children.size() == 0) {
             return;
@@ -179,27 +189,26 @@ public class MapViewFragment extends Fragment {
                 GeoLocation commentLocation = c.getLocation();
                 setGeoPointMarker(new GeoPoint(commentLocation.getLatitude(), commentLocation.getLongitude()));
                 checkCommentLocationDistance(c);
-                buildMarkersForChildComments(c);
+                handleChildComments(c);
             }
         }
     }
     
     public void checkCommentLocationDistance(Comment c) {
-        
-        double topCommentLat = topComment.getLocation().getLatitude();
-        double topCommentLong = topComment.getLocation().getLongitude();
         double commentLat = c.getLocation().getLatitude();
         double commentLong = c.getLocation().getLongitude();
-        Log.e("LatSpan", Double.toString(Math.abs(topCommentLat-commentLat)));
-        Log.e("LongSpan", Double.toString(Math.abs(topCommentLong-commentLong)));
         
-        
-        if (Math.abs(topCommentLat - commentLat) > DELTA_LAT) {
-            DELTA_LAT = Math.abs(topCommentLat - commentLat);
+        if (commentLat > MAX_LAT) {
+            MAX_LAT = commentLat;
         }
-        
-        if (Math.abs(topCommentLong - commentLong) > DELTA_LONG) {
-            DELTA_LONG = Math.abs(topCommentLong - commentLong);
+        if (commentLat < MIN_LAT) {
+            MIN_LAT = commentLat;
+        }
+        if (commentLong > MAX_LONG) {
+            MAX_LONG = commentLong;
+        }
+        if (commentLong < MIN_LONG) {
+            MIN_LONG = commentLong;
         }
     }
     
@@ -213,21 +222,18 @@ public class MapViewFragment extends Fragment {
         marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         openMapView.getOverlays().add(marker);
     }
+   
 
     /**
      * Called when the get_directions_button is clicked. Displays directions from
      * current location to the comment location. Uses an Async task to get map overlay
      */
-    public void getDirections() {
-        mapController.zoomToSpan(latSpan*2*90000, longSpan*2*90000);
-        Log.e("LatSpan", Double.toString(latSpan));
-        Log.e("LongSpan", Double.toString(longSpan));
-        /*
+    public void getDirections() {   
         if (currentLocation.getLocation() == null) {
             ErrorDialog.show(getActivity(), "Could not retrieve your location");
         } else {
             new MapAsyncTask().execute();
         }
-        openMapView.invalidate();*/
+        openMapView.invalidate();
     }
 }
