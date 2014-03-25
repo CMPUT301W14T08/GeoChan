b3fa70d625931e392415f54c87d0a97dd03ebcf3
hpabst <hpabst@ualberta.ca>
2014-03-25 08:39:59 -0600

    Created layout for EditCommentFragment.

---
+++ GeoChan/src/ca/ualberta/cmput301w14t08/geochan/fragments/EditCommentFragment.java
@@ -0,0 +1,28 @@
+package ca.ualberta.cmput301w14t08.geochan.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class EditCommentFragment extends Fragment {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState); 
+    }
+    
+    @Override
+    public void onStart(){
+        super.onStart();
+    }
+    
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+    
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+}
