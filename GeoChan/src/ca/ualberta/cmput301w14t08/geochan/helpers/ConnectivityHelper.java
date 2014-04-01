f6f84231b0730cff2ef8e935ee57ed5cab7f22c4
Artem Herasymchuk <artemh@gmail.com>
2014-04-01 13:49:37 -0600

    Beginning work on adapting for connectivity changes

---
+++ GeoChan/src/ca/ualberta/cmput301w14t08/geochan/helpers/ConnectivityHelper.java
@@ -0,0 +1,73 @@
+package ca.ualberta.cmput301w14t08.geochan.helpers;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Provides status about the user's current Internet connection.
+ * @author Artem Herasymchuk
+ *
+ */
+public class ConnectivityHelper {
+    
+    private static ConnectivityHelper instance = null;
+    private static ConnectivityManager connectivityManager;
+    
+    /**
+     * Constructor, sets up the connectivity manager.
+     * Private to avoid usage outside of as a singleton.
+     * @param context
+     */
+    private ConnectivityHelper(Context context) {
+        connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+    
+    /**
+     * Generates a singleton instance of this helper.
+     * @param context the context
+     */
+    public static void generateInstance(Context context) {
+        instance = new ConnectivityHelper(context);
+    }
+    
+    /**
+     * Returns the singleton instance of this helper
+     * @return the instance
+     */
+    public static ConnectivityHelper getInstance() {
+        return instance;
+    }
+
+    /**
+     * Returns true if device is connected to internet
+     * 
+     * @return the connection status
+     */
+    public boolean isConnected() {
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        return (activeNetwork != null && activeNetwork.isConnectedOrConnecting());
+    }
+
+    /**
+     * Returns true if device is connected via WiFi
+     * 
+     * @return the connection status
+     */
+    public boolean isWifi() {
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        return (isConnected() && (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI));
+    }
+
+    /**
+     * Returns true if device is connected via Mobile
+     * 
+     * @return the connection status
+     */
+    public boolean isMobile() {
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        return (isConnected() && (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE));
+    }
+    
+}
--- GeoChan/src/ca/ualberta/cmput301w14t08/geochan/helpers/ConnectivityListenerService.java
+++ GeoChan/src/ca/ualberta/cmput301w14t08/geochan/helpers/ConnectivityListenerService.java
@@ -3,75 +3,20 @@ package ca.ualberta.cmput301w14t08.geochan.helpers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.util.Log;
 
 public class ConnectivityListenerService extends BroadcastReceiver {
 
-    private ConnectivityManager connectivityManager;
-
-    /**
-     * Constructor. Gets the system service Connectivity Service
-     * 
-     * @param context
-     */
-    public ConnectivityListenerService(Context context) {
-        connectivityManager = (ConnectivityManager) context
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-    }
 
     /**
      * called when BroacastReceiver receives an intent broadcast
      */
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO -> Implement :)
-        Log.e("onReceive", "Called");
-    }
-
-    /**
-     * Returns true if device is connected to internet
-     * 
-     * @return isConnected
-     */
-    public boolean isConnected() {
-        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
-        return isConnected;
-    }
-
-    /**
-     * Returns true if device is connected via WiFi
-     * 
-     * @return isConnectedToWifi
-     */
-    public boolean isWifi() {
-        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
-        boolean isConnectedToWifi = activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
-        return isConnectedToWifi;
-    }
-
-    /**
-     * Returns true if device is connected via Mobile
-     * 
-     * @return isConnectedToMobile
-     */
-    public boolean isMobile() {
-        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
-        boolean isConnectedToMobile = activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE;
-        return isConnectedToMobile;
-    }
-
-    /**
-     * getters and setters
-     */
-
-    public ConnectivityManager getConnectivityManager() {
-        return connectivityManager;
-    }
-
-    public void setConnectivityManager(ConnectivityManager connectivityManager) {
-        this.connectivityManager = connectivityManager;
+        ConnectivityHelper helper = ConnectivityHelper.getInstance();
+        if (helper.isConnected()) {
+            // Internet connection is back after not having it
+        } else {
+            // Internet connection lost
+        }
     }
 }
