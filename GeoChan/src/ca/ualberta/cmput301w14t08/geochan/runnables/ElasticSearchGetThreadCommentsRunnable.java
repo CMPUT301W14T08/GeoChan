f99af0e6d00b9935682ef2bae6edbc1eb19250d0
Artem Herasymchuk <artemh@gmail.com>
2014-04-05 20:06:52 -0600

    Finished moving getting threads to thread manager

--- GeoChan/src/ca/ualberta/cmput301w14t08/geochan/runnables/ElasticSearchGetThreadCommentsRunnable.java
+++ GeoChan/src/ca/ualberta/cmput301w14t08/geochan/runnables/ElasticSearchGetThreadCommentsRunnable.java
@@ -1,19 +1,28 @@
 package ca.ualberta.cmput301w14t08.geochan.runnables;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 import io.searchbox.client.JestResult;
 import io.searchbox.core.Get;
+import io.searchbox.core.Search;
 import ca.ualberta.cmput301w14t08.geochan.helpers.ElasticSearchClient;
+import ca.ualberta.cmput301w14t08.geochan.helpers.ElasticSearchQueries;
 import ca.ualberta.cmput301w14t08.geochan.helpers.GsonHelper;
 import ca.ualberta.cmput301w14t08.geochan.models.CommentList;
+import ca.ualberta.cmput301w14t08.geochan.models.ElasticSearchResponse;
+import ca.ualberta.cmput301w14t08.geochan.models.ElasticSearchSearchResponse;
+import ca.ualberta.cmput301w14t08.geochan.models.ThreadComment;
 import ca.ualberta.cmput301w14t08.geochan.models.ThreadList;
 import ca.ualberta.cmput301w14t08.geochan.tasks.ElasticSearchGetThreadCommentsTask;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 public class ElasticSearchGetThreadCommentsRunnable implements Runnable {
     private ElasticSearchGetThreadCommentsTask task;
-    private String type = ElasticSearchClient.TYPE_INDEX;
+    private String type = ElasticSearchClient.TYPE_THREAD;
     public static final int STATE_GET_THREADS_FAILED = -1;
     public static final int STATE_GET_THREADS_RUNNING = 0;
     public static final int STATE_GET_THREADS_COMPLETE = 1;
@@ -28,23 +37,32 @@ public class ElasticSearchGetThreadCommentsRunnable implements Runnable {
         android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
         task.handleGetThreadCommentsState(STATE_GET_THREADS_RUNNING);
         JestResult result = null;
-        String id = ThreadList.getThreads().get(task.getThreadIndex()).getId();
         try {
             if (Thread.interrupted()) {
                 throw new InterruptedException();
             }
-            Get get = new Get.Builder(ElasticSearchClient.URL_INDEX, id).type(type)
-                    .build();
-            result = ElasticSearchClient.getInstance().getClient().execute(get);
+            String query = ElasticSearchQueries.SEARCH_MATCH_ALL;
+            Search search = new Search.Builder(query).addIndex(ElasticSearchClient.URL_INDEX).addType(type).build();
+            result = ElasticSearchClient.getInstance().getClient().execute(search);
             if (Thread.interrupted()) {
                 throw new InterruptedException();
             }
-            JsonObject object = result.getJsonObject().get("_source").getAsJsonObject();
+            Type elasticSearchSearchResponseType = new TypeToken<ElasticSearchSearchResponse<ThreadComment>>() {
+            }.getType();
+            Gson gson = GsonHelper.getOnlineGson();
+            ElasticSearchSearchResponse<ThreadComment> esResponse = gson.fromJson(result.getJsonString(), elasticSearchSearchResponseType);
             if (Thread.interrupted()) {
                 throw new InterruptedException();
             }
-            Gson gson = GsonHelper.getExposeGson();
-            CommentList list = gson.fromJson(object, CommentList.class);
+            ArrayList<ThreadComment> list = new ArrayList<ThreadComment>();
+            for (ElasticSearchResponse<ThreadComment> r : esResponse.getHits()) {
+                ThreadComment object = r.getSource();
+                list.add(object);
+            }
+            if (Thread.interrupted()) {
+                throw new InterruptedException();
+            }
+            ThreadList.setThreads(list);
             task.handleGetThreadCommentsState(STATE_GET_THREADS_COMPLETE);
         } catch (Exception e) {
             //
