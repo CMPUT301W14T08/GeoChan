c3c3a125d691d3c595ce80cb17b2d1a9fecd8c5b
bradleyjsimons <simons@ualberta.ca>
2014-04-07 15:16:10 -0600

    added an infoWindow test to customMarker tests

--- GeoChanTests/src/ca/ualberta/cmput301w14t08/geochan/test/CustomMarkerTest.java
+++ GeoChanTests/src/ca/ualberta/cmput301w14t08/geochan/test/CustomMarkerTest.java
@@ -3,18 +3,21 @@ package ca.ualberta.cmput301w14t08.geochan.test;
 import org.osmdroid.views.MapView;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import ca.ualberta.cmput301w14t08.geochan.activities.MainActivity;
+import ca.ualberta.cmput301w14t08.geochan.managers.ThreadManager;
 import ca.ualberta.cmput301w14t08.geochan.models.CustomMarker;
 import ca.ualberta.cmput301w14t08.geochan.models.GeoLocation;
-
 
 /**
  * J-unit test class for CustomMarker objects
  * 
  * @author Brad Simons
- *
+ * 
  */
 public class CustomMarkerTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
@@ -65,7 +68,7 @@ public class CustomMarkerTest extends ActivityInstrumentationTestCase2<MainActiv
         assertEquals("GeoPoint positions should be the same", geoLocation.makeGeoPoint(),
                 customMarker2.getPosition());
         assertEquals("Icons should be the same", icon, customMarker2.getIcon());
-        
+
         // test constructor 3
         CustomMarker customMarker3 = new CustomMarker(geoLocation.makeGeoPoint(), map, icon);
         assertEquals("Maps should be the same", map, customMarker3.getMapView());
@@ -76,5 +79,38 @@ public class CustomMarkerTest extends ActivityInstrumentationTestCase2<MainActiv
         assertEquals("GeoPoint positions should be the same", geoLocation.makeGeoPoint(),
                 customMarker3.getPosition());
         assertEquals("Icons should be the same", icon, customMarker3.getIcon());
+    }
+
+    /**
+     * Tests the setup of the info window. The info window of the marker will
+     * have a title string and the point of interest string in it after the 
+     * setup
+     */
+    public void testSetupInfoWindow() {
+        MapView map = new MapView(activity, null);
+        GeoLocation geoLocation = new GeoLocation(1.0, 1.0);
+
+        String title = "testTitle";
+
+        final CustomMarker customMarker1 = new CustomMarker(geoLocation, map);
+        customMarker1.setUpInfoWindow(title, activity);
+
+        final CustomMarker customMarker2 = new CustomMarker(geoLocation, map);
+        ProgressDialog dialog = new ProgressDialog(activity);
+        dialog.setMessage("Retrieving Location");
+        ThreadManager.startGetPOI(geoLocation, dialog, customMarker2);
+
+        assertEquals("titles should be the same", title, customMarker1.getTitle());
+
+        // check the POI
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            public void run() {
+                assertEquals("POI should be the same", customMarker1.getSubDescription(),
+                        customMarker2.getSubDescription());
+                Log.e("poi", customMarker1.getSubDescription());
+            }
+        };
+        handler.postDelayed(runnable, 2000);
     }
 }
